5d87fe1e407f4df6befaa7d53c7c426a2a392679
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2020-12-03 15:47:58 -0600

    refactor day three solution to remove redundant code and improve readability

--- src/main/kotlin/daythree/DayThree.kt
+++ src/main/kotlin/daythree/DayThree.kt
@@ -1,35 +1,33 @@
 package daythree
 
-class DayThree(private val originX: Int = 0,
-               private val originY: Int = 0,
-               private val deltaX: Int,
-               private val deltaY: Int) {
+class DayThree(
+  private val originX: Int = 0,
+  private val originY: Int = 0,
+  private val deltaX: Int,
+  private val deltaY: Int
+) {
   private val data = DayThreeData.data
     .split('\n')
     .map { it.trim() }
-  private val maxHeightIdx: Int = data.size - 1
 
   companion object {
     const val TREE = '#'
   }
 
-  fun getNumberOfTrees(): Int {
-    return getNumberOfTrees(originX, originY, 0)
-  }
+  fun getNumberOfTrees() = countTrees(originX, originY, 0)
 
-  private fun getNumberOfTrees(colIdx: Int, rowIdx: Int, numberOfTrees: Int): Int {
-    if (colIdx > maxHeightIdx) {
+  private fun countTrees(colIdx: Int, rowIdx: Int, numberOfTrees: Int): Int {
+    if (colIdx > data.lastIndex) {
       return numberOfTrees
     }
 
     val newTreeCount = if (data[colIdx][rowIdx] == TREE) numberOfTrees + 1 else numberOfTrees
 
-    val nextRowIdx = if (rowIdx + deltaX > data[colIdx].lastIndex) rowIdx + deltaX - data[colIdx].length else rowIdx + deltaX
+    val isNextRowIndexGreaterThanRowLastIndex = rowIdx + deltaX > data[colIdx].lastIndex
+    val nextRowIdx =
+      if (isNextRowIndexGreaterThanRowLastIndex) rowIdx + deltaX - data[colIdx].length else rowIdx + deltaX
 
-    return if (colIdx == 0 && rowIdx == 0) {
-      getNumberOfTrees(colIdx + deltaY, deltaX, newTreeCount)
-    } else {
-      getNumberOfTrees(colIdx + deltaY, nextRowIdx, newTreeCount)
-    }
+
+    return countTrees(colIdx + deltaY, nextRowIdx, newTreeCount)
   }
 }
