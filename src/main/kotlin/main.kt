7d21a0ffc41ef550ea54c1deb588e32f76810952
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2020-12-03 15:41:14 -0600

    refactor part two solution to be a little cleaner

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,9 +14,12 @@ fun main(args: Array<String>) {
   println()
 
   println("Day Three Advent of Code 2020 Part 1 Solution - ${DayThree(deltaX = 3, deltaY = 1).getNumberOfTrees()}")
-  val partTwoProblems = listOf(Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1), Pair(1, 2))
-  val partTwoSolution = partTwoProblems
-    .map { DayThree(deltaX = it.first, deltaY = it.second).getNumberOfTrees().toBigInteger() }
-    .reduce { acc, bigInteger -> acc * bigInteger }
+  val partTwoSlopes = listOf(Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1), Pair(1, 2))
+  val partTwoSolution = partTwoSlopes.fold(1.toBigInteger()) { acc, pair ->
+    DayThree(
+      deltaX = pair.first,
+      deltaY = pair.second
+    ).getNumberOfTrees().toBigInteger() * acc
+  }
   println("Day Three Advent of Code 2020 Part 2 Solution - $partTwoSolution")
 }
